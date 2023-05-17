e2ceb03b977e4776d5e6f82c607d5eb5f58a1ffe
Roman Parfentev <romanntev@gmail.com>
2023-05-17 15:06:32 +0200

    edit structure

--- 05-17-2023/Objects/Lev2_1/main.js
+++ 05-17-2023/Objects/Lev2_1/main.js
@@ -1,56 +1,65 @@
-const singers = [
-  {
-    name: "The Beatles",
-    country: "United Kingdom",
-    period_active: { start: 1960, end: 1970 },
-    genre: "Rock / Pop",
-  },
-  {
-    name: "Elvis Presley",
-    country: "United States",
-    period_active: { start: 1954, end: 1977 },
-    genre: "Rock and roll",
-  },
-  {
-    name: "Michael Jackson",
-    country: "United States",
-    period_active: { start: 1964, end: 2009 },
-    genre: "Pop / Rock / Dance / Soul / R&B",
-  },
-  {
-    name: "Elton John",
-    country: "United Kingdom",
-    period_active: { start: 1964, end: "present" },
-    genre: "Pop / Rock",
-  },
-  {
-    name: "Madonna",
-    country: "United States",
-    period_active: { start: 1979, end: "present" },
-    genre: "Pop / Dance / Electronica",
-  },
-  {
-    name: "Led Zeppelin",
-    country: "United Kingdom",
-    period_active: { start: 1968, end: 1980 },
-    genre: "Hard rock / Blues rock / Folk rock",
-  },
-  {
-    name: "Rihanna",
-    country: "United States",
-    period_active: { start: 2005, end: "present" },
-    genre: "R&B / Pop / Dance / Hip-hop",
-  },
-  {
-    name: "Pink Floyd",
-    country: "United Kingdom",
-    period_active: { start: 1965, end: 1996, extra: 2014 },
-    genre: "Progressive rock / Psychedelic rock",
-  },
+let edelMetallPreise = [
+  { name: "Gold", preiseGramEuro: 42.91, veraenderung: "+0.12%" },
+  { name: "Silber", preiseGramEuro: 0.51, veraenderung: "+1.02%" },
+  { name: "Platin", preiseGramEuro: 25.74, veraenderung: "+0.41%" },
+  { name: "Palladium", preiseGramEuro: 50.93, veraenderung: "0.00%" },
+  { name: "Rhodium", preiseGramEuro: 160.12, veraenderung: "-0.10%" },
+  { name: "Iridium", preiseGramEuro: 42.84, veraenderung: "0.00%" },
+  { name: "Ruthenium", preiseGramEuro: 7.36, veraenderung: "0.00%" },
+  { name: "Rhenium", preiseGramEuro: 37.22, veraenderung: "-0.53%" },
+  { name: "Osmium", preiseGramEuro: 11.54, veraenderung: "0.00%" },
 ];
 
-singers.sort(function (a, b) {
-  if (a.name < b.name) return -1;
+const newArrayName = [];
+edelMetallPreise.forEach((elm) => {
+  return newArrayName.push(elm.name);
+});
+console.log(newArrayName);
+
+edelMetallPreise.map((elm) => {
+  console.log(elm.name);
 });
 
-console.log(singers);
+let newArrayPreise = [];
+edelMetallPreise.forEach((elm) => {
+  return newArrayPreise.push(elm.preiseGramEuro);
+});
+console.log(newArrayPreise);
+
+edelMetallPreise.map((elm) => {
+  console.log(elm.preiseGramEuro);
+});
+
+const newArrayVeraenderung = [];
+edelMetallPreise.forEach((elm) => {
+  return newArrayVeraenderung.push(elm.veraenderung);
+});
+console.log(newArrayVeraenderung);
+
+edelMetallPreise.map((elm) => {
+  console.log(elm.veraenderung);
+});
+
+const priceOver50 = newArrayPreise.filter(checkPrice);
+function checkPrice(price) {
+  return price > 50;
+}
+console.log(priceOver50);
+
+//! ====== TABELLE ======
+
+const createTable = () => {
+  let table = document.createElement("table");
+
+  let content = `<tr><th>${Object.keys(edelMetallPreise[0])[0]}</th><th>${
+    Object.keys(edelMetallPreise[0])[1]
+  }</th><th>${Object.keys(edelMetallPreise[0])[2]}</th></tr>`;
+
+  for (var i = 0; i < edelMetallPreise.length; i++) {
+    content += `<tr><td>${edelMetallPreise[i].name}</td><td>${edelMetallPreise[i].preiseGramEuro}</td><td>${edelMetallPreise[i].veraenderung}</td></tr>`;
+  }
+  table.innerHTML = content;
+
+  document.body.appendChild(table);
+};
+createTable();
