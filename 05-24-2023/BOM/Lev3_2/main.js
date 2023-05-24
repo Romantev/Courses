47d4a9ef784d46abe9ea0cfa17a3255e52d755c9
Roman Parfentev <romanntev@gmail.com>
2023-05-24 13:42:59 +0200

    edit function

--- 05-24-2023/BOM/Lev3_2/main.js
+++ 05-24-2023/BOM/Lev3_2/main.js
@@ -2,21 +2,25 @@ let outputNumb = document.querySelector("#time");
 let inputVal = Number(document.querySelector("#minutes").value);
 let countDown;
 let secVal = 1;
+let globalscope = false;
 
 function startMinCountdown() {
-  inputVal = Number(document.querySelector("#minutes").value);
-  countDown = setInterval(() => {
-    secVal--;
-    if (secVal < 0) {
-      secVal = 59;
-      inputVal--;
-    }
-    let sec = secVal < 10 ? "0" + secVal : secVal;
-    outputNumb.innerHTML = `${inputVal}:${sec}`;
-    if (inputVal === 0 && secVal == 0) {
-      clearTimeout(countDown);
-    }
-  }, 1000);
+  if (globalscope == false) {
+    inputVal = Number(document.querySelector("#minutes").value);
+    countDown = setInterval(() => {
+      secVal--;
+      if (secVal < 0) {
+        secVal = 59;
+        inputVal--;
+      }
+      let sec = secVal < 10 ? "0" + secVal : secVal;
+      outputNumb.innerHTML = `${inputVal}:${sec}`;
+      if (inputVal === 0 && secVal == 0) {
+        clearTimeout(countDown);
+      }
+      globalscope = true;
+    }, 1000);
+  }
 }
 
 function pauseMinCountdown() {
